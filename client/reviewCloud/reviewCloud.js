8eb2a3735bd6fd3eb383d9a29b88b17b79c64314
n-white <neil.r.whitehead@gmail.com>
2016-07-12 15:44:36 -0700

    removed common words

--- client/reviewCloud/reviewCloud.js
+++ client/reviewCloud/reviewCloud.js
@@ -4,7 +4,7 @@ angular.module('reviewCloudApp.reviewCloud', ['reviewCloudApp.searchPlaces'])
 	.controller('ReviewCloudController', function($scope, $state, $http, searchId) {
 		$scope.reviewsArray = [];
 		$scope.noDuplicates;
-		$scope.removeWords = ['youd', 'their', 'theirs']
+		// $scope.removeWords = ['youd', 'their', 'theirs']
 	
 		/////////
 		$scope.getCurrentId = function() {
@@ -57,7 +57,7 @@ angular.module('reviewCloudApp.reviewCloud', ['reviewCloudApp.searchPlaces'])
 
 	    var color = d3.scale.linear()
 	            .domain([100, 20, 15, 10, 6, 5, 4, 3, 2, 1, 0])
-	            .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
+	            .range(["#676767", "#676767", "#676767", "#676767", "#0099ff", "#00ffcc", "#99ff33", "#ccff33", "#ffcc00", "#ff9933", "#ff3300", "#ff0000"]);
 
 	    d3.layout.cloud().size([800, 300])
 	            .words(frequency_list)
@@ -88,7 +88,30 @@ angular.module('reviewCloudApp.reviewCloud', ['reviewCloudApp.searchPlaces'])
 
 		}
 
+		$scope.removeWords = ['​it​','​I​','​you​','​he​','​they​','​we​','​she​','​who​','​them​','​me​','​him​','​one​','​her​','​us​','​something​','​nothing​','anything',
+		'himself','everything','someone','themselves','everyone','itself','anyone','myself','food','another','any','anybody','anyone',
+		'anything','both','each','other','either','everybody','everyone','everything','few','he','her','her','herself','him','himself','his','i',
+		'it','its','itself','little','many','me','mine','more','most','much','my','myself','neither','no','one','nobody','none','nothing','another','other','others','our',
+		'ours','ourselves','several','she','some','somebody','someone','something','that','their','theirs','them','themselves','these','they','this',
+		'those','us','we','what','whatever','which','whichever','who','whoever','whom','womever','whose','you','your','yours','youre','yourself','yourselves','up',
+		'so','out','just','now','how','then','more','also','here','well','only','very','even','back','there','down','still','in','as','too','when','never',
+		'really','most','be','have','do','say','get','make','go','know','take','see','come','think','look','want','give','use','find','tell',
+		'ask','work','seem','feel','try','leave','call','up','so','out','just','now','how','then','more','also','here','well','only','very',
+		'really','most','be','have','do','say','get','make','go','know','take','see','come','think','​look​','​want​','​give​','​use​','find','tell',
+		'​even​','​back​','​there​','​down​','​still​','​in​','​as​','​too​','​when​','​never​','​ask​','​work​','​seem​','​feel​','​try​','​leave​','​call','​up​','so​','out​','just​','now​','how​','then​','more​','also​','here​','well​','only​','very​',
+		'even​','back​','there​','down​','still​','in​','as​','too​','when​','never​','really​','most​','of​','in​','to​','for​','with​',
+		'on​','at​','from​','by​','about​','as​','into​','like​','through​','after​','over​','between​','out​','against​','during​','without​','before​','under​','around​','among​',
+		'and​','that​','but​','or​','as​','into​','like​','through​','after​','over​','between​','out​','against​','during​','without​','before​','under​',
+		'around​','among​','and​','that​','but​','or​','as​','if​','when​','than​','because​','while​','where​','after​','so​','though​','since​','until​',
+		'whether​','before​','although​','nor​','like​','once​','unless​','now​','two​','first​','last​','three​','next​','million​','four​','five​','second​',
+		'six​','third​','billion​','hundred​','thousand​','seven​','eight​','ten​','nine​','dozen​','fourth​','twenty​','fifth​','thirty​','yes','oh','yeah',
+		'no','hey','hi','hello','ah']
+
+		
+
 	});
+
+
 
 
 
